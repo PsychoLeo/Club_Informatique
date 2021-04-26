6ee16f125134865a313a061254a4e071422ce0b7
PsychoLeo <leopold.bernard.leo@gmail.com>
2021-04-26 11:47:18 +0200

    commit all command

--- 9-Chess/utils/settings.py
+++ 9-Chess/utils/settings.py
@@ -1,5 +1,5 @@
 settings = {
-    "piecesFolder":"fantasy", # There are merida, spatial and fantasy
+    "piecesFolder":"merida", # There are merida, spatial and fantasy
     "boardFolder":"wood",
     "whereToSave":"gameName.txt", #The party will be saved in the folder game with the name in value here
     "whoPlaysWhite":"TheNameOfWhoPlaysWhites",
